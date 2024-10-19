b262111cd73526b163aab68cf026da57eb4591ea
Ben Cornick <35277154+bencornick@users.noreply.github.com>
2024-10-18 21:24:59 -0500

    Update mockDatabase.js

--- database/mockDatabase.js
+++ database/mockDatabase.js
@@ -3,23 +3,23 @@
 let users = [];
 let events = [];
 
+
 const User = {
-  findOne: jest.fn(email => Promise.resolve(users.find(user => user.email === email))),
   create: jest.fn(data => {
-    const newUser = { ...data, events: [] };
+    const newUser = { id: users.length + 1, ...data };
     users.push(newUser);
     return Promise.resolve(newUser);
   }),
-  find: jest.fn(() => Promise.resolve(users)),
-  findById: jest.fn(id => Promise.resolve(users.find(user => user._id === id))),
-  update: jest.fn((id, data) => {
-    const user = users.find(user => user._id === id);
+  findByUsername: jest.fn(username => Promise.resolve(users.find(user => user.username === username))),
+  update: jest.fn((username, data) => {
+    const user = users.find(user => user.username === username);
     if (user) {
       Object.assign(user, data);
       return Promise.resolve(user);
     }
     return Promise.reject(new Error('User not found'));
   }),
+  findAll: jest.fn(() => Promise.resolve(users)),
   clear: () => {
     users = [];
   },
