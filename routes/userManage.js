b8892e1dc24e3f5d81fc9fc2f3e004a595f71f4a
Ben Cornick <35277154+bencornick@users.noreply.github.com>
2024-11-22 15:53:38 -0600

    Update userManage.js

--- routes/userManage.js
+++ routes/userManage.js
@@ -1,20 +1,6 @@
 //userManage.js
-
 const express = require('express');
 const router = express.Router();
 const {manage} = require ('../controllers/userManagementController');
-
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      const updatedUserProfile = await manage(req.body);
-      res.status(201).json({ message: 'Profile updated successfully', data: updatedUserProfile });
-    } catch (error) {
-      console.error('Error updating profile:', error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+router.post('/manage', manage);
+module.exports = router;
